0bfb11f7fde6acffb3fa77e763c1a063aadc608c
mmario.prog <mmario.prog@gmail.com>
2026-05-01 22:09:55 +0200

    cancella files

--- FavStations.js
+++ FavStations.js
@@ -1,11 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'FavStations',
-<<<<<<< HEAD
     version: '0.0.7',
-=======
-    version: '0.0.6',
->>>>>>> 71adc1cf4c3baace0266d3a536e39aa5e194fbcc
     author: 'mm-prg',
     frontEndPath: 'FavStations/pluginFavStations.js'
 } 
